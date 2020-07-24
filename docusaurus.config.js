ca20bd2d9d4b2fac3b302c2e16d2a353c654e266
technicky <technicky22@yahoo.com>
2020-07-24 15:46:20 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'My Site',
+  title: 'Welcome to Creative Rebel',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
